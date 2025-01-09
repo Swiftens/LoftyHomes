13e72f7caf00faf191f19b8d421f0029db5b0fa4
Swiftens <swiftens26@gmail.com>
2025-01-09 16:13:04 +0800

    Initial Commit
    Putting things out

---
+++ src/main/java/me/swiftens/loftyHomes/LoftyHomes.java
@@ -0,0 +1,17 @@
+package me.swiftens.loftyHomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LoftyHomes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
